a7ec2f72a9ed7b2dfe01d279e8c26c66e3dda644
Blessings Minga <113256890+BlessingsMinga@users.noreply.github.com>
2025-06-05 15:12:22 +0200

    Create Campus Map Activity code

---
+++ app/src/main/java/com/example/mycampusassistant/CampusMapActivity.java
@@ -0,0 +1,8 @@
+public class CampusMapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campus_map);
+        // ImageView is static, no additional logic needed
+    }
+}
